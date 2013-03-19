298f012ecd95a54e4fd9ba45be767934e27e3402
Bastien LEMALE <blemale@excilys.com>
2013-03-19 22:31:08 +0100

    Add ExternalTodoLight api entity.

---
+++ src/main/java/fr/blemale/dropwizard/todo/api/ExternalTodoLight.java
@@ -0,0 +1,32 @@
+package fr.blemale.dropwizard.todo.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ExternalTodoLight {
+    @JsonProperty
+    private final long id;
+    @JsonProperty
+    private final String title;
+    @JsonProperty
+    private final String selfUrl;
+
+    @JsonCreator
+    public ExternalTodoLight(@JsonProperty("id") long id, @JsonProperty("title") String title, @JsonProperty("selfUrl") String selfUrl) {
+        this.id = id;
+        this.title = title;
+        this.selfUrl = selfUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSelfUrl() {
+        return selfUrl;
+    }
+}
