1be178396b8d24a8d7b443d256d60db5117fa6e9
Bastien LEMALE <blemale@excilys.com>
2013-03-19 22:12:31 +0100

    Add validation constraint @NotNull to Todo.title.

--- src/main/java/fr/blemale/dropwizard/todo/core/Todo.java
+++ src/main/java/fr/blemale/dropwizard/todo/core/Todo.java
@@ -1,7 +1,10 @@
 package fr.blemale.dropwizard.todo.core;
 
+import javax.validation.constraints.NotNull;
+
 public class Todo {
     private final long id;
+    @NotNull
     private final String title;
     private final String content;
 
@@ -21,5 +24,27 @@ public class Todo {
 
     public String getContent() {
         return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Todo todo = (Todo) o;
+
+        if (id != todo.id) return false;
+        if (content != null ? !content.equals(todo.content) : todo.content != null) return false;
+        if (!title.equals(todo.title)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        return result;
     }
 }
