169162bedb2bb74f0d0c444960bd451a396a100a
Bastien LEMALE <blemale@excilys.com>
2013-03-19 22:16:22 +0100

    Add TodoUpdateRequest api entity.

---
+++ src/main/java/fr/blemale/dropwizard/todo/api/TodoUpdateRequest.java
@@ -0,0 +1,33 @@
+package fr.blemale.dropwizard.todo.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+
+public class TodoUpdateRequest {
+    @NotNull
+    @JsonProperty
+    private final long id;
+    @JsonProperty
+    private final String title;
+    @JsonProperty
+    private final String content;
+
+    public TodoUpdateRequest(@JsonProperty("id") long id, @JsonProperty("title") String title, @JsonProperty("content") String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
