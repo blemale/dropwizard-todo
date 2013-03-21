5a03ba427833ed9df7d32d78f2cd8b7b53aad725
Bastien LEMALE <blemale@excilys.com>
2013-03-21 20:22:16 +0100

    Add test for TodoCreationRequest Json deserialization.

---
+++ src/test/java/fr/blemale/dropwizard/todo/api/todo/request/TodoCreationRequestTest.java
@@ -0,0 +1,26 @@
+package fr.blemale.dropwizard.todo.api.todo.request;
+
+import org.junit.Test;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.fromJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TodoCreationRequestTest {
+    @Test
+    public void consumesTheExpectedJsonWithAllFields() throws Exception {
+        TodoCreationRequest todoCreationRequest = new TodoCreationRequest("title", "content");
+        assertThat("parsing a valid API representation produces a person",
+                fromJson(jsonFixture("fixtures/todoCreationRequest.json"), TodoCreationRequest.class),
+                is(todoCreationRequest));
+    }
+
+    @Test
+    public void consumesTheExpectedJsonWithMandatoryFields() throws Exception {
+        TodoCreationRequest todoCreationRequest = new TodoCreationRequest("title", null);
+        assertThat("parsing a valid API representation with only mandatory fields produces a person",
+                fromJson(jsonFixture("fixtures/todoCreationRequestWithOnlyMandatory.json"), TodoCreationRequest.class),
+                is(todoCreationRequest));
+    }
+}
