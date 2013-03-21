48bd14aa91bdc64e6ee5c50c1cd1f14b2326d427
Bastien LEMALE <blemale@excilys.com>
2013-03-21 20:04:33 +0100

    Add test for externalTodo Json serialization.

---
+++ src/test/java/fr/blemale/dropwizard/todo/api/todo/external/ExternalTodoTest.java
@@ -0,0 +1,19 @@
+package fr.blemale.dropwizard.todo.api.todo.external;
+
+import org.junit.Test;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.asJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ExternalTodoTest {
+    @Test
+    public void producesTheExpectedJson() throws Exception {
+        ExternalTodo externalTodoList = new ExternalTodo(1L, "title", "content", "selfUrl");
+
+        assertThat("rendering a externalTodo as JSON produces a valid API representation",
+                asJson(externalTodoList),
+                is(jsonFixture("fixtures/externalTodo.json")));
+    }
+}
