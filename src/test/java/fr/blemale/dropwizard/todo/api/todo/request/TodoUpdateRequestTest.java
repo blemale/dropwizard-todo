e9fd7089a0171ac1182f257935bc7d516079f221
Bastien LEMALE <blemale@excilys.com>
2013-03-21 20:28:21 +0100

    Add test for TodoUpdateRequest Json deserialization.

---
+++ src/test/java/fr/blemale/dropwizard/todo/api/todo/request/TodoUpdateRequestTest.java
@@ -0,0 +1,26 @@
+package fr.blemale.dropwizard.todo.api.todo.request;
+
+import org.junit.Test;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.fromJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TodoUpdateRequestTest {
+    @Test
+    public void consumesTheExpectedJsonWithAllFields() throws Exception {
+        TodoUpdateRequest todoUpdateRequest = new TodoUpdateRequest(1L, "title", "content");
+        assertThat("parsing a valid API representation produces a TodoUpdateRequest",
+                fromJson(jsonFixture("fixtures/todoUpdateRequest.json"), TodoUpdateRequest.class),
+                is(todoUpdateRequest));
+    }
+
+    @Test
+    public void consumesTheExpectedJsonWithMandatoryFields() throws Exception {
+        TodoUpdateRequest todoUpdateRequest = new TodoUpdateRequest(1, null, null);
+        assertThat("parsing a valid API representation with only mandatory fields produces a TodoUpdateRequest",
+                fromJson(jsonFixture("fixtures/todoUpdateRequestWithOnlyMandatory.json"), TodoUpdateRequest.class),
+                is(todoUpdateRequest));
+    }
+}
