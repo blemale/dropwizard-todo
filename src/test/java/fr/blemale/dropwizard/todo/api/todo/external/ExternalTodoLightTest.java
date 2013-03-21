88cb8e46ba94a08f8706fcef16f01f0f6d015e8a
Bastien LEMALE <blemale@excilys.com>
2013-03-21 20:10:39 +0100

    Add test for externalTodoLight Json serialization.

---
+++ src/test/java/fr/blemale/dropwizard/todo/api/todo/external/ExternalTodoLightTest.java
@@ -0,0 +1,19 @@
+package fr.blemale.dropwizard.todo.api.todo.external;
+
+import org.junit.Test;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.asJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ExternalTodoLightTest {
+    @Test
+    public void producesTheExpectedJson() throws Exception {
+        ExternalTodoLight externalTodoLight = new ExternalTodoLight(1L, "title", "selfUrl");
+
+        assertThat("rendering a externalTodoLight as JSON produces a valid API representation",
+                asJson(externalTodoLight),
+                is(jsonFixture("fixtures/externalTodoLight.json")));
+    }
+}
