ba62f8874cf02fd1023bbcf15fa3916f1c32a2d0
Bastien LEMALE <blemale@excilys.com>
2013-03-24 23:49:40 +0100

    Set up TodoResource in TodoService.

--- src/main/java/fr/blemale/dropwizard/todo/TodoService.java
+++ src/main/java/fr/blemale/dropwizard/todo/TodoService.java
@@ -3,16 +3,20 @@ package fr.blemale.dropwizard.todo;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import fr.blemale.dropwizard.todo.jdbi.DummyTodoDAO;
+import fr.blemale.dropwizard.todo.jdbi.TodoDAO;
+import fr.blemale.dropwizard.todo.resources.TodoResource;
 
 public class TodoService extends Service<TodoConfiguration> {
 
     @Override
     public void initialize(Bootstrap<TodoConfiguration> bootstrap) {
-        // TODO
+        bootstrap.setName("todo");
     }
 
     @Override
     public void run(TodoConfiguration configuration, Environment environment) throws Exception {
-        // TODO
+        final TodoDAO todoDAO = new DummyTodoDAO();
+        environment.addResource(new TodoResource(todoDAO));
     }
 }
