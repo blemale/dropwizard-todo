c94e3f3764c7b46a8369d5d77d0a9a4e9389a2c9
Bastien LEMALE <blemale@excilys.com>
2013-03-24 17:46:12 +0100

    Remove TodoService and Refactoring of TodoResource.

--- src/main/java/fr/blemale/dropwizard/todo/resources/TodoResource.java
+++ src/main/java/fr/blemale/dropwizard/todo/resources/TodoResource.java
@@ -7,7 +7,8 @@ import fr.blemale.dropwizard.todo.api.todo.external.ExternalTodoLight;
 import fr.blemale.dropwizard.todo.api.todo.external.ExternalTodoList;
 import fr.blemale.dropwizard.todo.api.todo.request.TodoCreationRequest;
 import fr.blemale.dropwizard.todo.api.todo.request.TodoUpdateRequest;
-import fr.blemale.dropwizard.todo.core.TodoService;
+import fr.blemale.dropwizard.todo.core.Todo;
+import fr.blemale.dropwizard.todo.jdbi.TodoDAO;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -19,28 +20,29 @@ import javax.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON)
 public class TodoResource {
 
-    private final TodoService todoService;
+    private final TodoDAO todoDAO;
 
-    public TodoResource(TodoService todoService) {
-        this.todoService = todoService;
+    public TodoResource(TodoDAO todoDAO) {
+        this.todoDAO = todoDAO;
     }
 
     @GET
     public ExternalTodoList getTodos() {
-        return this.todoService.getTodos();
+        return new ExternalTodoList.Mapper().fromTodoList(this.todoDAO.getTodos());
     }
 
     @POST
     public ExternalTodoLight createTodo(@Valid TodoCreationRequest todoCreationRequest) {
-        return this.todoService.createTodo(todoCreationRequest);
+        Todo createdTodo = this.todoDAO.createTodo(new TodoCreationRequest.Mapper().toTodo(todoCreationRequest));
+        return new ExternalTodoLight.Mapper().fromTodo(createdTodo);
     }
 
     @Path("{id}")
     @GET
     public ExternalTodo getTodo(@PathParam("id") LongParam id) {
-        Optional<ExternalTodo> todo = this.todoService.getTodo(id.get());
+        Optional<Todo> todo = this.todoDAO.getTodo(id.get());
         if (todo.isPresent()) {
-            return todo.get();
+            return new ExternalTodo.Mapper().fromTodo(todo.get());
         } else {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
@@ -49,6 +51,12 @@ public class TodoResource {
     @Path("{id}")
     @PUT
     public ExternalTodoLight updateTodo(@PathParam("id") LongParam id, @Valid TodoUpdateRequest todoUpdateRequest) {
-        return this.updateTodo(id, todoUpdateRequest);
+        Todo updatedTodo = new TodoUpdateRequest.Mapper().toTodo(id.get(), todoUpdateRequest);
+        Optional<Todo> todo = this.todoDAO.updateTodo(updatedTodo);
+        if (todo.isPresent()) {
+            return new ExternalTodoLight.Mapper().fromTodo(todo.get());
+        } else {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
     }
 }
