9d0dd5e93730a5a3dceca9da8f4e22089d5d3c75
Bastien LEMALE <blemale@excilys.com>
2013-03-25 22:35:55 +0100

    Set up auth provider in the service.

--- src/main/java/fr/blemale/dropwizard/todo/TodoConfiguration.java
+++ src/main/java/fr/blemale/dropwizard/todo/TodoConfiguration.java
@@ -1,6 +1,19 @@
 package fr.blemale.dropwizard.todo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yammer.dropwizard.config.Configuration;
+import org.hibernate.validator.constraints.NotBlank;
 
 public class TodoConfiguration extends Configuration {
+    @NotBlank
+    @JsonProperty
+    private final String password;
+
+    public TodoConfiguration(@JsonProperty("password") String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/fr/blemale/dropwizard/todo/TodoService.java
+++ src/main/java/fr/blemale/dropwizard/todo/TodoService.java
@@ -1,8 +1,11 @@
 package fr.blemale.dropwizard.todo;
 
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.auth.basic.BasicAuthProvider;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import fr.blemale.dropwizard.todo.auth.DummyAuthenticator;
+import fr.blemale.dropwizard.todo.core.User;
 import fr.blemale.dropwizard.todo.jdbi.DummyTodoDAO;
 import fr.blemale.dropwizard.todo.jdbi.TodoDAO;
 import fr.blemale.dropwizard.todo.resources.TodoResource;
@@ -19,6 +22,9 @@ public class TodoService extends Service<TodoConfiguration> {
 
     @Override
     public void run(TodoConfiguration configuration, Environment environment) throws Exception {
+        final String password = configuration.getPassword();
+        environment.addProvider(new BasicAuthProvider<User>(new DummyAuthenticator(password), "Protect Area"));
+
         final TodoDAO todoDAO = new DummyTodoDAO();
         environment.addResource(new TodoResource(todoDAO));
     }
